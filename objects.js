e41ef976206c2d5dfa7353b9039662c621bec618
Andy Striph <ajstriph@me.com>
2017-10-05 19:59:57 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ var updatePlaylist = function (playlist, artistName, songTitle){
   playlist['Phil Ochs']="Here's to the State of Mississippi";
   return playlist;
 }
+var removeFromPlaylist = function (playlist, artistName){
+var playlist = {["Kanye"] = "Gold Digger"};
+delete playlist.artistName;
+  return playlist;
+}
