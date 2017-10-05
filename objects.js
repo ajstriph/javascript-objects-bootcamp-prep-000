2c4f60fe18e9397a612ac93d6f9e695e39750e3f
Andy Striph <ajstriph@me.com>
2017-10-05 19:31:22 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {artistName: "songTitle"};
 
 var updatePlaylist = function (playlist, artistName, songTitle){
-  playlist.artistName=songTitle;
+  playlist = {artistName: "songTitle"}
   return playlist;
 }
 updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi");
