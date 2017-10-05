caf20efea130068073d7c487436310ab4b2d0ffd
Andy Striph <ajstriph@me.com>
2017-10-05 19:17:50 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,4 @@
 var playlist = {artistName: "songTitle"};
+function updatePlaylist (playlist, 'artistName', "songTitle"){
+  return updatePlaylist (playlist, 'Phil Ochs', "Here's to the State of Mississippi");
+}
