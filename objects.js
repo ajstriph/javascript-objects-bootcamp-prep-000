c1c01b814087c0536305bb755a133dc3383e364c
Andy Striph <ajstriph@me.com>
2017-10-05 20:09:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ var updatePlaylist = function (playlist, artistName, songTitle){
   return playlist;
 }
 var removeFromPlaylist = function (playlist, artistName){
-playlist['Kanye'] = "Gold Digger";
-delete playlist.['Kanye'];
+artistName = {['Kanye']: "Gold Digger"};
+delete playlist.artistName;
   return playlist;
 }
